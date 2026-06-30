b678682757125891577b88c32070899439b69f6b
youngnationagenda <youngnationagenda@gmail.com>
2026-06-30 14:59:26 +0300

    auto: sync changes

--- src/providers/Web3Provider.tsx
+++ src/providers/Web3Provider.tsx
@@ -19,9 +19,6 @@ createWeb3Modal({
   featuredWalletIds: [
     'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
   ],
-  includeWalletIds: [
-    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
-  ],
 })
 
 interface Web3ProviderProps {
