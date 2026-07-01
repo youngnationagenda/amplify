de90cb4d9a20632d31c23bcd58f1bc30aaee1ace
youngnationagenda <youngnationagenda@gmail.com>
2026-07-01 11:34:02 +0300

    fix: correct amplify_outputs.json import path and update Node.js versions

--- apps/nettribexyz/src/integrations/amplify/config.ts
+++ apps/nettribexyz/src/integrations/amplify/config.ts
@@ -1,5 +1,5 @@
 import { Amplify } from 'aws-amplify';
-import outputs from '../../../amplify_outputs.json';
+import outputs from '../../../../amplify_outputs.json';
 
 /**
  * Configure Amplify with generated outputs.
